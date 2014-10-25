84c3f4f7203b1563406562283d749c9f8be2ddbd
cgunski <cgunski@a545c3e3-68c1-7eb3-1e26-e304427a8aa4>
2014-10-25 16:09:49 +0000

    Se agrega el tratamiento de excepciones y reglas para los cuantificadores.

--- projects/tesis/src/ar/uba/dc/formalex/fl/regulation/formula/connectors/FLQuantifier.java
+++ projects/tesis/src/ar/uba/dc/formalex/fl/regulation/formula/connectors/FLQuantifier.java
@@ -1,5 +1,10 @@
 package ar.uba.dc.formalex.fl.regulation.formula.connectors;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
+import ar.uba.dc.formalex.fl.bgtheory.Agente;
+import ar.uba.dc.formalex.fl.bgtheory.BGUtil;
 import ar.uba.dc.formalex.fl.regulation.formula.FLFormula;
 
 
@@ -24,5 +29,21 @@ public abstract class FLQuantifier extends FLFormula{
 		this.role = role;
 		this.variable = variable;
         this.formula = formula;
+        this.exceptions = new HashSet();
 	}
+    
+    protected FLFormula exceptionsInstantiator(FLFormula rule, Agente agent, BGUtil bgUtil){
+    	if (exceptions != null && !exceptions.isEmpty()){ // se realiza la disyunción de todos los Permisos que son excepciones a la regla
+        	Iterator<FLFormula> exceptionsFormulas = exceptions.iterator();
+        	FLFormula exceptionFormOr = exceptionsFormulas.next(); 
+        	while (exceptionsFormulas.hasNext()){
+        		FLFormula next = exceptionsFormulas.next();
+        		exceptionFormOr = new FLOr(exceptionFormOr, next);
+        	}
+        	//instancio con los agentes la conjunción de los Permisos que representan la excepción.
+        	FLFormula exceptionFormInst = exceptionFormOr.instanciar(variable, agent.getName(), bgUtil);     	        	
+        	rule = new FLOr(rule, exceptionFormInst);
+        }
+    	return rule;
+    }
 }
