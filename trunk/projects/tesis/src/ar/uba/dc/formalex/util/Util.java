0d89d48ecfdd7ae6f32ed83125fa72b46c8074e5
melisa.raiczyk@gmail.com <melisa.raiczyk@gmail.com@a545c3e3-68c1-7eb3-1e26-e304427a8aa4>
2015-03-07 20:37:35 +0000

    Agrego al util el concepto de powerset pero que toma como input un set de set de T y retorna lo mismo.

--- trunk/projects/tesis/src/ar/uba/dc/formalex/util/Util.java
+++ trunk/projects/tesis/src/ar/uba/dc/formalex/util/Util.java
@@ -45,6 +45,25 @@ public class Util {
         }
         return sets;
     }
+    
+    public static <T> Set<Set<T>> powerSetSet(Set<Set<T>> originalSet) {
+        Set<Set<T>> sets = new HashSet<Set<T>>();
+        if (originalSet.isEmpty()) {
+            sets.add(new HashSet<T>());
+            return sets;
+        }
+        List<Set<T>> list = new ArrayList<Set<T>>(originalSet);
+        Set<T> head = list.get(0);
+        Set<Set<T>> rest = new HashSet<Set<T>>(list.subList(1, list.size()));
+        for (Set<T> set : powerSetSet(rest)) {
+            Set<T> newSet = new HashSet<T>();            
+            newSet.addAll(head);
+            newSet.addAll(set);
+            sets.add(newSet);
+            sets.add(set);            
+        }
+        return sets;
+    }
 
     /**
      * Lee las propiedades del archivo y las asigna a System.properties
