7e9444af635ea7bb90069b2ca892804ca62effbe
melisa.raiczyk@gmail.com <melisa.raiczyk@gmail.com@a545c3e3-68c1-7eb3-1e26-e304427a8aa4>
2015-05-16 12:52:57 +0000

    Nuevo tipo de permiso permanente para expresar aquellos permisos que deben suceder de forma permanente en una traza.

---
+++ trunk/projects/tesis/src/ar/uba/dc/formalex/fl/regulation/permission/PermanentPermission.java
@@ -0,0 +1,27 @@
+package ar.uba.dc.formalex.fl.regulation.permission;
+
+import ar.uba.dc.formalex.fl.bgtheory.BGUtil;
+import ar.uba.dc.formalex.fl.regulation.formula.FLFormula;
+import ar.uba.dc.formalex.fl.regulation.rules.Obligation;
+
+public class PermanentPermission extends FLFormula {
+	private FLFormula formula;
+	
+	public PermanentPermission(FLFormula formula) {
+        //O(P( fórmula ))
+        Permission permission = new Permission(formula);
+        this.formula =  new Obligation(permission);        
+    }
+
+	@Override
+	public String toString() {
+		return formula.toString();
+	}
+
+	@Override
+	public FLFormula instanciar(String variable, String agente, BGUtil bgUtil,
+			Boolean forceAgent) {
+		return formula.instanciar(variable, agente, bgUtil, forceAgent);
+	}
+
+}
