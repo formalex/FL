9fe826c0c75b97d5c5d43e60a0651e31e69805b5
cfaciano <carlos.faciano@gmail.com>
2016-06-08 14:21:02 -0300

    Agregué una clase de test para unificar en un solo lugar los ejemplos para la presentacion
    Se agregó un ejemplo para el reductor y el reemplazo de JH

---
+++ test/ar/uba/dc/formalex/ui/EjemplosParaLaPresentacion.java
@@ -0,0 +1,37 @@
+package ar.uba.dc.formalex.ui;
+
+import org.junit.Test;
+
+import ar.uba.dc.formalex.fl.Formalex;
+import ar.uba.dc.formalex.fl.regulation.formula.LTLTranslationType;
+import ar.uba.dc.formalex.fl.utils.FlTest;
+
+/**
+ * Incluí ejemplos que se van a usar en la presentación
+ * @author cfaciano
+ *
+ */
+public class EjemplosParaLaPresentacion extends FlTest {
+
+	private static final String EJ_PARA_LA_PRESENTACION_TXT = "ejParaLaPresentacion.txt";
+
+	/******************* EJS para el REDUCTOR y el REEMPLAZO DE JH **********/
+	@Test
+	public void conFiltradoSinReductor() throws Exception {
+		
+		Formalex.run(ROOT_EJS_PARA_EL_REDUCTOR + EJ_PARA_LA_PRESENTACION_TXT, CORRER_CON_MODEL_CHECKER , CORRER_CON_FILTRADO, false, LTLTranslationType.WITH_JH);
+	}
+	
+	@Test
+	public void conFiltradoConReductor() throws Exception {
+		
+		Formalex.run(ROOT_EJS_PARA_EL_REDUCTOR + EJ_PARA_LA_PRESENTACION_TXT, CORRER_CON_MODEL_CHECKER, CORRER_CON_FILTRADO, true, LTLTranslationType.WITH_JH);
+	}
+	
+	@Test
+	public void conFiltradoSinReductorConReemplazoDeJH() throws Exception {
+		
+		Formalex.run(ROOT_EJS_PARA_EL_REDUCTOR + EJ_PARA_LA_PRESENTACION_TXT, CORRER_CON_MODEL_CHECKER, CORRER_CON_FILTRADO, true, LTLTranslationType.WITH_NEXT_FOR_JH);
+	}
+	
+}
